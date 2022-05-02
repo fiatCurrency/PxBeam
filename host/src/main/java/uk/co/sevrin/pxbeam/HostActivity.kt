bc6fc25309fadee9650af7dec8d996eae1165515
= <=>
2022-05-03 00:15:41 +0100

    Move the UpdaterThread() into a class/file of its own. We make it internal, so it can be seen only within this module.

--- host/src/main/java/uk/co/sevrin/pxbeam/HostActivity.kt
+++ host/src/main/java/uk/co/sevrin/pxbeam/HostActivity.kt
@@ -1,7 +1,6 @@
 package uk.co.sevrin.pxbeam
 
 import android.os.Bundle
-import android.os.Handler
 import android.os.Looper
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -61,36 +60,5 @@ class HostActivity : AppCompatActivity() {
         Log.d(tag,"<<onTick()")
     }
 
-    inner class UpdaterThread(
-        looper: Looper,
-        private val sleepMillis: Long,
-        private val func: ()->Unit) : Thread()
-    {
-        private val handler = Handler(looper)
-        private val tag = "UpdaterThread()"
-        @Volatile private var exitRequest = false
-
-        fun stopSynchronously() {
-            exitRequest = true
-            this.interrupt()
-            this.join()
-        }
-
-        override fun run() {
-            Log.d(tag,"Infinite loop entered")
-            while (!exitRequest) {
-                try {
-                    Log.d(tag,"Posting runnable to handler")
-                    handler.post(func)
-                    Log.d(tag, "Entering sleep")
-                    sleep(sleepMillis)
-                    Log.d(tag, "Sleep exited normally")
-                } catch (e: InterruptedException) {
-                    Log.d(tag, "Sleep interrupted")
-                }
-            }
-            Log.d(tag,"Infinite loop exited")
-        }
-    }
 
 }
