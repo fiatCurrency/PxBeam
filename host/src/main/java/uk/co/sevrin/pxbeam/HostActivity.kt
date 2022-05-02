d0e0cbee7bc3e662fa14e77552bd8fe6c6fa9601
= <=>
2022-05-02 22:31:39 +0100

    Weirdness

--- host/src/main/java/uk/co/sevrin/pxbeam/HostActivity.kt
+++ host/src/main/java/uk/co/sevrin/pxbeam/HostActivity.kt
@@ -1,7 +1,7 @@
 package uk.co.sevrin.pxbeam
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class HostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
