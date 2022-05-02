e872718b86432824882e0d195a6bef741b3d9d83
= <=>
2022-05-02 23:30:30 +0100

    Add the inner class UpdaterThread
    This implements a single run() member function, which repeatedly
    posts a given function (as a Runnable) to the (Handler of the)
    given Looper, every so many milli seconds.
    
    This was implemented as a class derived from Thread, rather than
    as a Runnable that woudl eventually be wrapped in a Thread constructor,
    because I wanted to have some visibility of the Thread, to be able to
    stop it co-operatively.

--- host/src/main/java/uk/co/sevrin/pxbeam/HostActivity.kt
+++ host/src/main/java/uk/co/sevrin/pxbeam/HostActivity.kt
@@ -1,6 +1,8 @@
 package uk.co.sevrin.pxbeam
 
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
@@ -53,3 +55,29 @@ class HostActivity : AppCompatActivity() {
         Log.d(tag,"<<onTick()")
     }
 
+    inner class UpdaterThread(
+        looper: Looper,
+        private val sleepMillis: Long,
+        private val func: ()->Unit) : Thread()
+    {
+        private val handler = Handler(looper)
+        private val tag = "UpdaterThread()"
+        @Volatile private var exitRequest = false
+        override fun run() {
+            Log.d(tag,"Infinite loop entered")
+            while (!exitRequest) {
+                try {
+                    Log.d(tag,"Posting runnable to handler")
+                    handler.post(func)
+                    Log.d(tag, "Entering sleep")
+                    sleep(sleepMillis)
+                    Log.d(tag, "Sleep exited normally")
+                } catch (e: InterruptedException) {
+                    Log.d(tag, "Sleep interrupted")
+                }
+            }
+            Log.d(tag,"Infinite loop exited")
+        }
+    }
+
+}
