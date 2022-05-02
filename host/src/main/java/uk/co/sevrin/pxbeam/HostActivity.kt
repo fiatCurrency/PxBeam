af6f9bf504b52397e5f2ba8258c2dab47c886a49
= <=>
2022-05-02 23:17:01 +0100

    Write a more fully-featured onCreate() handler: Logging on entry/exit, and initialisation of the TextView that will hold the display of the time

--- host/src/main/java/uk/co/sevrin/pxbeam/HostActivity.kt
+++ host/src/main/java/uk/co/sevrin/pxbeam/HostActivity.kt
@@ -1,11 +1,19 @@
 package uk.co.sevrin.pxbeam
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import android.widget.TextView
 
 class HostActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
+
+    private val tag = "HostActivity"
+    private lateinit var txv: TextView
+    override fun onCreate(sis: Bundle?) {
+        Log.d(tag,">>onCreate()")
+        super.onCreate(sis)
         setContentView(R.layout.activity_host)
+        txv = this.findViewById(R.id.txvTime)
+        Log.d(tag,"<<onCreate()")
     }
 }
